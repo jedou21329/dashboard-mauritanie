731a6eaeda9027d5b75d0f01154eaf3c5ba0c5e6
jedou21329 <jdydousins29@gmail.com>
2026-01-21 00:19:23 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1106,7 +1106,7 @@ elif page == "📊 Analyses Avancées":
                     yaxis_title="Chômage (%)",
                     zaxis_title="Croissance (%)"
                 ),
-                height=600
+                height=300
             )
             st.plotly_chart(fig, use_container_width=True, config=plotly_config)
         else:
